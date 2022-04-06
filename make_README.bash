bedcd1d899d0eb91f3b0a68784391c3314f2ecbd
yrom1 <ryanm.inbox@gmail.com>
2022-04-05 22:52:23 -0400

    Testing making `README.md` with current scraped jobs

--- make_README.bash
+++ make_README.bash
@@ -1,4 +1,5 @@
 set -e
 rm -f README.md
-python3 -m nbconvert --to markdown README.ipynb
+python3 -m nbconvert --to markdown read_local_pickle.nbconvert.ipynb
+mv read_local_pickle.nbconvert.md README.md
 python3 clean_README.py
