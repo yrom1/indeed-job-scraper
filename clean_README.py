0829bf02d7fcba5e4bd65a51c790764a78cff147
yrom1 <ryanm.inbox@gmail.com>
2022-04-17 04:15:05 -0400

    Add new style tag to clean from nbconverted README

--- clean_README.py
+++ clean_README.py
@@ -8,6 +8,7 @@ def main() -> None:
         README = f.read()
 
     README_no_script_tag = re.sub("<style scoped>(.|\n)*?<\/style>", "", README)
+    README_no_script_tag = re.sub("<style type=\"text/css\">(.|\n)*?<\/style>", "", README)
 
     with open("README.md", "w") as f:
         f.write(README_no_script_tag)
